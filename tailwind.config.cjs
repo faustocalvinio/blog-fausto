ca79771b2b162c2e71e3ba1cfb42ad13ae89c077
Fausto Calviño <faustocalvino@outlook.com>
2023-11-16 20:39:41 +0100

    colours configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
 		fontFamily: {
 			sans: ['Comfortaa', 'sans-serif'],
+		},		
+		colors: {
+			gray: colors.gray,
+			white: colors.white,
+			red: colors.red,
+			black: colors.black,
 		},
-		
 		extend: {},
 	},
 	plugins: [],
