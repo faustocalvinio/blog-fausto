149451020022472b1111b20f9754d7e6c78405f4
Fausto Calviño <faustocalvino@outlook.com>
2023-11-23 16:40:58 +0100

    general configurations

--- content/blog/setup-a-react-project.md
+++ content/blog/setup-a-react-project.md
@@ -4,6 +4,5 @@ draft: false
 title: How to set up a React project?
 description: It's a beautiful world out there.
 date: 2023-11-05
-readingMinutes: 'undefined'
 ---
 lorem
--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,8 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Blog Fausto Calviño";
 export const SITE_DESCRIPTION =
   "Welcome to my blog! I write mostly about my personal learning day by day.";
 export const TWITTER_HANDLE = "@faustocalvinio";
 export const MY_NAME = "Fausto Calviño";
 
-// setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
 export const SITE_URL = BASE_URL.origin;
